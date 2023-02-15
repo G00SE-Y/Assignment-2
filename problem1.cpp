a008758afac712ce6b5a4e57cc4d1adbaf49a4d9
Ethan <bulldawg0065@gmail.com>
2023-02-15 18:05:57 -0500

    Initialized repo.

---
+++ problem1.cpp
@@ -0,0 +1,10 @@
+/*
+ *  Minotaur's Birthday Party
+ *
+ *  Author: Ethan Woollet
+ *  Date: Feb 2023 
+ * 
+ *  Professor: Juan Parra
+ *  Class: Parallel and Distributed Processing (COP4520)
+ * 
+ */
---
+++ problem2.cpp
@@ -0,0 +1,10 @@
+/*
+ *  Minotaur's Crystal Vase
+ *
+ *  Author: Ethan Woollet
+ *  Date: Feb 2023 
+ * 
+ *  Professor: Juan Parra
+ *  Class: Parallel and Distributed Processing (COP4520)
+ * 
+ */
